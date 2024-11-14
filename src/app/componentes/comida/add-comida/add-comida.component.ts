29c1bee17ab55b63f2bbbdbc51b312e987caf0dc
mordmora <danielbonnett20@gmail.com>
2024-11-13 23:07:12 -0500

    ???

--- src/app/componentes/comida/add-comida/add-comida.component.ts
+++ src/app/componentes/comida/add-comida/add-comida.component.ts
@@ -1,12 +1,17 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-add-comida',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './add-comida.component.html',
   styleUrl: './add-comida.component.css'
 })
 export class AddComidaComponent {
+  addMovie() {
+    throw new Error('Method not implemented.');
+  }
 
 }
