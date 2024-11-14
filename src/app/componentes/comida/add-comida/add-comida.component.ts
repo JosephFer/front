e603a29b8620701bb084a9e00fc6bbf9d075fd2b
AndresMes <federicoescobar2022@gmail.com>
2024-11-13 23:05:37 -0500

    Agregado serviciio en addComidaTs

--- src/app/componentes/comida/add-comida/add-comida.component.ts
+++ src/app/componentes/comida/add-comida/add-comida.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Comida } from '../../../interface/comida';
+import { ComidaService } from '../../../services/api/comida/comida.service';
 
 @Component({
   selector: 'app-add-comida',
@@ -8,5 +10,25 @@ import { Component } from '@angular/core';
   styleUrl: './add-comida.component.css'
 })
 export class AddComidaComponent {
+  nuevaComida : Comida = {
+    idMenu : 0,
+    precio : 0,
+    cantidadDisponible : 0,
+    idComida : 0,
+    idCine : 0
+  }
 
+  constructor(private comidaService : ComidaService) {}
+
+  crearComida() {
+    this.comidaService.addComida(this.nuevaComida).subscribe(
+      response => {
+        console.log('Comida creada:', response);
+        // Aquí puedes hacer algo después de crear la comida, como mostrar un mensaje o limpiar el formulario
+      },
+      error => {
+        console.error('Error al crear la comida:', error);
+      }
+    );
+  }
 }
